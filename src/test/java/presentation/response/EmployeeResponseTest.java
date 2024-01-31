d3ab85967c0f64f7299bec77503bb8cf42ce00e1
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-01-31 22:30:19 +0900

    EmployeeResponseTestにパターンを追加。

--- src/test/java/presentation/response/EmployeeResponseTest.java
+++ src/test/java/presentation/response/EmployeeResponseTest.java
@@ -7,11 +7,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeResponseTest {
     @Test
-    void 任意のEmployeeに対してEmployeeResponseが作成できる() {
+    void 山田太郎というEmployeeに対してEmployeeResponseが作成できる() {
         Employee employee = new Employee("1", "Taro", "Yamada");
         EmployeeResponse actual =  EmployeeResponse.createResponse(employee);
 
         EmployeeResponse expected = new EmployeeResponse("1", "Taro", "Yamada");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void 山田次郎というEmployeeに対してEmployeeResponseが作成できる() {
+        Employee employee = new Employee("2", "Jiro", "Yamada");
+        EmployeeResponse actual =  EmployeeResponse.createResponse(employee);
+
+        EmployeeResponse expected = new EmployeeResponse("2", "Jiro", "Yamada");
 
         assertEquals(expected, actual);
     }
