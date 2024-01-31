7f7afe9c813522e895e96e09c9a305695c557e79
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-01-31 22:42:10 +0900

    EmployeeResponseTestが成功するようにEmployeeResponseを修正。Javadocの追記。

--- src/main/java/domain/Employee.java
+++ src/main/java/domain/Employee.java
@@ -1,4 +1,11 @@
 package domain;
 
+/**
+ * 従業員を表すオブジェクト。
+ *
+ * @param id        従業員を識別する個別のID
+ * @param firstName 従業員の名前
+ * @param lastName  従業員の名字
+ */
 public record Employee(String id, String firstName, String lastName) {
 }
--- src/main/java/presentation/response/EmployeeResponse.java
+++ src/main/java/presentation/response/EmployeeResponse.java
@@ -2,8 +2,25 @@ package presentation.response;
 
 import domain.Employee;
 
+/**
+ * 従業員を表すレスポンス用のオブジェクト。
+ *
+ * @param id        従業員のID
+ * @param firstName 従業員の名前
+ * @param lastName  従業員の名字
+ */
 public record EmployeeResponse(String id, String firstName, String lastName) {
+    /**
+     * EmployeeResponseを作成します。
+     *
+     * @param employee 従業員オブジェクト
+     * @return 作成されたEmployeeResponse
+     */
     public static EmployeeResponse createResponse(Employee employee) {
-        return new EmployeeResponse("1", "Taro", "Yamada");
+        return new EmployeeResponse(
+                employee.id(),
+                employee.firstName(),
+                employee.lastName()
+        );
     }
 }
