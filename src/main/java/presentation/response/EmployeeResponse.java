55fe67cb2339492013c2172f0325bbdfb7fdf6e3
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-01-31 22:28:04 +0900

    EmployeeResponseの実装、EmployeeResponseTestの成功。

--- src/main/java/presentation/response/EmployeeResponse.java
+++ src/main/java/presentation/response/EmployeeResponse.java
@@ -1,4 +1,9 @@
 package presentation.response;
 
+import domain.Employee;
+
 public record EmployeeResponse(String id, String firstName, String lastName) {
+    public static EmployeeResponse createResponse(Employee employee) {
+        return new EmployeeResponse("1", "Taro", "Yamada");
+    }
 }
