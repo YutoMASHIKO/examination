05ab932cfc6868bfa2145a496a3db2f046983ab1
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-06 23:48:50 +0900

    EmployeeRestControllerTestの作成。

--- src/main/java/domain/Employee.java
+++ src/main/java/domain/Employee.java
@@ -1,7 +1,7 @@
 package domain;
 
 /**
- * 従業員を表すオブジェクト。
+ * 従業員を表すオブジェクト.
  *
  * @param id        従業員を識別する個別のID
  * @param firstName 従業員の名前
---
+++ src/main/java/presentation/controller/EmployeeRestController.java
@@ -0,0 +1,7 @@
+package presentation.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeRestController {
+}
