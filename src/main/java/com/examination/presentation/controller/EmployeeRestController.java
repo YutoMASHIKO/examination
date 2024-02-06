511764c7d72ae2a3bb6a756a2a767bc654341ce9
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-07 00:12:17 +0900

    EmployeeRestControllerへJavadocの追加。

--- src/main/java/com/examination/presentation/controller/EmployeeRestController.java
+++ src/main/java/com/examination/presentation/controller/EmployeeRestController.java
@@ -1,15 +1,23 @@
 package com.examination.presentation.controller;
 
 import com.examination.domain.Employee;
+import com.examination.presentation.response.EmployeeResponse;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import com.examination.presentation.response.EmployeeResponse;
 
-import java.util.List;
-
+/**
+ * 従業員情報を管理するためのコントローラー.
+ */
 @RestController
 public class EmployeeRestController {
+
+  /**
+   * すべての従業員を取得します.
+   *
+   * @return <{@link EmployeeResponse}>.
+   */
   @GetMapping("v1/employees")
   @ResponseBody
   public List<EmployeeResponse> getAllEmployees() {
