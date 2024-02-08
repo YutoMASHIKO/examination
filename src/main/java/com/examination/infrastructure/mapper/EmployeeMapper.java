c0bc204151737c3f4eb55550c032177eb65c83d6
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-08 23:19:14 +0900

    EmployeeMapperの全件取得を実装。

--- src/main/java/com/examination/infrastructure/mapper/EmployeeMapper.java
+++ src/main/java/com/examination/infrastructure/mapper/EmployeeMapper.java
@@ -1,7 +1,12 @@
 package com.examination.infrastructure.mapper;
 
+import com.examination.domain.Employee;
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface EmployeeMapper {
+  @Select("SELECT id, first_name, last_name FROM employees")
+  List<Employee> getAllEmployees();
 }
