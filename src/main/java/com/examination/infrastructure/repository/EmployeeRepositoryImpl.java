6491cc47e112f1d22e6d526d4c22d1c2899ece35
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-10 18:37:39 +0900

    EmployeeRepositoryに全件検索を実装。

--- src/main/java/com/examination/infrastructure/repository/EmployeeRepositoryImpl.java
+++ src/main/java/com/examination/infrastructure/repository/EmployeeRepositoryImpl.java
@@ -2,12 +2,18 @@ package com.examination.infrastructure.repository;
 
 import com.examination.domain.Employee;
 import com.examination.domain.EmployeeRepository;
+import com.examination.infrastructure.mapper.EmployeeMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
+@RequiredArgsConstructor
 public class EmployeeRepositoryImpl implements EmployeeRepository {
+  private final EmployeeMapper employeeMapper;
   @Override
   public List<Employee> getAllEmployees() {
-    return null;
+    return employeeMapper.getAllEmployees();
   }
 }
