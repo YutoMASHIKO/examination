ee953af16a145df13e081497daeb26cb07e5f51f
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-11 13:56:45 +0900

    EmployeeEntityの実装。

--- src/main/java/com/examination/infrastructure/entity/EmployeeEntity.java
+++ src/main/java/com/examination/infrastructure/entity/EmployeeEntity.java
@@ -1,5 +1,9 @@
 package com.examination.infrastructure.entity;
 
-public record EmployeeEntity(String id, String firstName, String lastName) {
+import com.examination.domain.Employee;
 
+public record EmployeeEntity(String id, String firstName, String lastName) {
+  public Employee convert() {
+    return new Employee(id, firstName, lastName);
+  }
 }
