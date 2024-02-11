3fe2bbba83deb3ac1650258cc24f532746ec5a49
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-11 17:10:43 +0900

    UpdateEmployeeUseCaseを修正。

--- src/main/java/com/examination/application/UpdateEmployeeUseCase.java
+++ src/main/java/com/examination/application/UpdateEmployeeUseCase.java
@@ -1,4 +1,20 @@
 package com.examination.application;
 
+import com.examination.application.data.UpdateEmployeeData;
+import com.examination.domain.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UpdateEmployeeUseCase {
+  private final EmployeeRepository employeeRepository;
+
+  public void update(UpdateEmployeeData updateEmployeeData) {
+    employeeRepository.updateEmployee(
+        updateEmployeeData.convert(
+            employeeRepository.getEmployeeById(updateEmployeeData.id())
+        )
+    );
+  }
 }
--- src/test/java/com/examination/application/UpdateEmployeeUseCaseTest.java
+++ src/test/java/com/examination/application/UpdateEmployeeUseCaseTest.java
@@ -1,13 +1,15 @@
 package com.examination.application;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import com.examination.application.data.UpdateEmployeeData;
+import com.examination.domain.Employee;
 import com.examination.domain.EmployeeRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 class UpdateEmployeeUseCaseTest {
   @InjectMocks
@@ -16,12 +18,21 @@ class UpdateEmployeeUseCaseTest {
   @Mock
   private EmployeeRepository employeeRepository;
 
+  @BeforeEach
+  void setup() {
+    MockitoAnnotations.openMocks(this);
+  }
+
   @Test
   void 従業員の更新を行う場合() {
-    UpdateEmployeeData updateEmployeeData = new UpdateEmployeeData("1", "Taro", "Yamada");
+    when(employeeRepository.getEmployeeById("1"))
+      .thenReturn(new Employee("1", "Taro", "Yamada"));
 
-//    sut.update(updateEmployeeData);
+    UpdateEmployeeData updateEmployeeData = new UpdateEmployeeData("1", "Ichiro", "Tanaka");
 
-//    verify(employeeRepository, times(1)).updateEmployee(null);
+    sut.update(updateEmployeeData);
+
+    verify(employeeRepository, times(1))
+      .updateEmployee(updateEmployeeData.convert(new Employee("1", "Taro", "Yamada")));
   }
 }
