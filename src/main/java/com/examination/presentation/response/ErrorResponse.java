650ab4b89b45c2815f099bba330584c7e8c9a6f2
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-11 19:48:46 +0900

    ErrorResponseの作成。

---
+++ src/main/java/com/examination/presentation/response/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.examination.presentation.response;
+
+import java.util.List;
+
+/**
+ * エラーレスポンスを表すレコードクラスです.
+ *
+ * @param code    エラーコード
+ * @param message エラーメッセージ
+ * @param details エラーの詳細情報のリスト
+ */
+public record ErrorResponse(String code, String message, List<String> details) {
+}
