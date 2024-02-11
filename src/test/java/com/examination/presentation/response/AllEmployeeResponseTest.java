831634b5b9c48b2b5d5816a8fade00def3931f4d
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-11 12:55:50 +0900

    AllEmployeeResponseTestを作成。

---
+++ src/test/java/com/examination/presentation/response/AllEmployeeResponseTest.java
@@ -0,0 +1,30 @@
+package com.examination.presentation.response;
+
+import com.examination.domain.Employee;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AllEmployeeResponseTest {
+  @Test
+  void 全件取得用の従業員レスポンスが生成できる() {
+    List<Employee> employeeList = List.of(
+      new Employee("1", "Taro", "Yamada"),
+      new Employee("2", "Jiro", "Yamada")
+    );
+
+    AllEmployeeResponse expected = new AllEmployeeResponse(
+      List.of(
+        new EmployeeResponse("1", "Taro", "Yamada"),
+        new EmployeeResponse("2", "Jiro", "Yamada")
+      )
+    );
+
+    AllEmployeeResponse actual = AllEmployeeResponse.createResponse(employeeList);
+
+    assertEquals(expected, actual);
+  }
+
+}
