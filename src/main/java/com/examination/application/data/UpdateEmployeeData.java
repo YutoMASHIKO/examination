ea3627712da5261472455811f03f8d8c36253d1a
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-11 16:49:47 +0900

    UpdateEmployeeDataの実装。

--- src/main/java/com/examination/application/data/UpdateEmployeeData.java
+++ src/main/java/com/examination/application/data/UpdateEmployeeData.java
@@ -4,6 +4,6 @@ import com.examination.domain.Employee;
 
 public record UpdateEmployeeData(String id, String firstName, String lastName) {
   public Employee convert(Employee employee) {
-    return null;
+    return new Employee(id, firstName, lastName);
   }
 }
