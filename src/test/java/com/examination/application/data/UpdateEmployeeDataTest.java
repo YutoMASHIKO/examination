b4ef704c6807f812c4711d37f751a495e587cbe5
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-11 16:50:27 +0900

    UpdateEmployeeDataTestに姓のみ更新のテストを追加。

--- src/test/java/com/examination/application/data/UpdateEmployeeDataTest.java
+++ src/test/java/com/examination/application/data/UpdateEmployeeDataTest.java
@@ -18,4 +18,16 @@ class UpdateEmployeeDataTest {
 
     assertEquals(expected, actual);
   }
+
+  @Test
+  void 姓のみ更新する場合() {
+    UpdateEmployeeData sut = new UpdateEmployeeData("1", null, "Tanaka");
+    Employee originalEmployee = new Employee("1", "Jiro", "Yamada");
+
+    Employee expected = new Employee("1", "Jiro", "Tanaka");
+
+    Employee actual = sut.convert(originalEmployee);
+
+    assertEquals(expected, actual);
+  }
 }
