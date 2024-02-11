2f14fb0a345c956cb3d6e2b38f4b28fe87fa80d1
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-11 22:14:30 +0900

    バリデーションエラーに関する例外処理を追加。

--- src/main/java/com/examination/common/GlobalExceptionHandler.java
+++ src/main/java/com/examination/common/GlobalExceptionHandler.java
@@ -4,7 +4,11 @@ import static java.util.Collections.emptyList;
 
 import com.examination.application.exception.EmployeeNotFoundException;
 import com.examination.presentation.response.ErrorResponse;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,7 +21,7 @@ public class GlobalExceptionHandler {
   /**
    * idによる検索に失敗した場合の例外ハンドリングメソッドです.
    *
-   * @param e {@link EmployeeNotFoundException} インスタンス
+   * @param e idによる検索に失敗した場合の例外
    * @return エラーレスポンス
    */
   @ExceptionHandler(EmployeeNotFoundException.class)
@@ -28,5 +32,26 @@ public class GlobalExceptionHandler {
       String.format("specified employee [id = %s] is not found.", e.getId()),
       emptyList()
       );
+  }
+
+  /**
+   * リクエストの入力に問題があった場合の例外ハンドリングメソッドです.
+   *
+   * @param e リクエストの入力に問題があった場合に発生する例外
+   * @return  バリデーションエラーに関する詳細情報を含むエラーレスポンス
+   */
+  @ExceptionHandler
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ErrorResponse handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+    List<String> details = new ArrayList<>();
+
+    for (FieldError error : e.getFieldErrors()) {
+      details.add(String.format("%s %s", error.getField(), error.getDefaultMessage()));
+    }
+    return new ErrorResponse(
+      "0002",
+      "request validation error is occurred.",
+      details
+    );
   }
 }
