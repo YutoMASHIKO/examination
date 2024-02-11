82cd2165e6dcbd672b9a7bd3a7e85665ee225f8a
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-11 14:10:47 +0900

    Mapperに次の従業員IDを取得を実装。

--- src/main/java/com/examination/infrastructure/mapper/EmployeeMapper.java
+++ src/main/java/com/examination/infrastructure/mapper/EmployeeMapper.java
@@ -5,6 +5,7 @@ import com.examination.infrastructure.entity.EmployeeEntity;
 import java.util.List;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
@@ -14,6 +15,10 @@ public interface EmployeeMapper {
 
   @Select("SELECT id, first_name, last_name FROM employees WHERE id = #{id}")
   EmployeeEntity getEmployeeById(String id);
+
+  @Select("SELECT nextval('EMPLOYEE_ID_SEQ')")
+  @Options(flushCache = Options.FlushCachePolicy.TRUE)
+  Long getNextId();
 
   @Insert("INSERT INTO employees (id, first_name, last_name) VALUES(#{id}, #{firstName}, #{lastName})")
   Integer insert(EmployeeEntity employeeEntity);
